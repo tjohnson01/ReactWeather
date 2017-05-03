4bb6c5527352530a95c1cf81fcc3511ae41ebf41
Travis Johnson <tjohnson01@gmail.com>
2017-05-03 11:45:10 -0600

    Adding heroku https redirect

--- server.js
+++ server.js
@@ -5,7 +5,16 @@ var express = require('express');
 var app = express();
 const PORT = process.env.PORT || 3000;
 
-
+if(process.env.PORT){
+  app.use(function (req, res, next){
+    if(req.headers['x-forwarded-proto'] === 'http'){
+      next();
+    }
+    else {
+      res.redirect('http://' + req.hostname + req.url);
+    }
+  });
+}
 
 app.use(express.static('public'));
 
